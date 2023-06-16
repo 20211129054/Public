3d1c45180d1cb0f28c0eebc28ddf8ab718726c8b
20211129054 <119372470+20211129054@users.noreply.github.com>
2023-06-16 21:14:13 +0300

    INT.PROG.2-FINAL

---
+++ uyg_vz/uyg_vz/ViewModel/CevapModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace uyg_vz.ViewModel
+{
+    public class CevapModel
+    {
+        public string cevapId { get; set; }
+        public string cevapSecenekId { get; set; }
+        public string cevapSecenek { get; set; }
+        public string cevapUyeId { get; set; }
+        public System.DateTime cevaplamaZamani { get; set; }
+        public CevapModel cevapBilgi { get; set; }
+        public UyeModel uyeBilgi { get; set; }
+    }
+}
---
+++ uyg_vz/uyg_vz/ViewModel/SoruModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace uyg_vz.ViewModel
+{
+    public class SoruModel
+    {
+        public string soruId { get; set; }
+        public string soruAnketId { get; set; }
+        public string soru { get; set; }
+        public int soruSecenekSayisi { get; set; }
+        public SoruModel soruBilgi { get; set; }
+        public AnketModel anketBilgi { get; set; }
+        public List<SecenekModel> secenekler { get; set; }
+    }
+}
