d830330e54e85490b59a7db0ee7078c70ade52c6
20211129054 <119372470+20211129054@users.noreply.github.com>
2023-06-16 21:28:41 +0300

    INT.PROG.2-FINAL

---
+++ uyg_vz/uyg_vz/ViewModel/SecenekModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace uyg_vz.ViewModel
+{
+    public class SecenekModel
+    {
+        public string secenekId { get; set; }
+        public string secenekSoruId { get; set; }
+        public string secenek { get; set; }
+        public int secenekSayisi { get; set; }
+        public SecenekModel secenekBilgi { get; set; }
+        public SoruModel soruBilgi { get; set; }
+        public int cevapSayisi { get; set; }
+    }
+}
